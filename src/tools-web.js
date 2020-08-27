c4b45dce704c1c5988a3cb10dc568c0d3083638a
yuanbiao <biao.yuan@jsxfedu.com>
2020-08-27 13:41:34 +0800

    新增 - 截图并保存为pdf文件

--- src/tools-web.js
+++ src/tools-web.js
@@ -226,7 +226,7 @@ const getUrlParams = (url) => {
  * @param {Function} callback 回调函数
  * @param {Boolean} isBubbling 是否冒泡
  * @param {Boolean} cancelable 是否阻止浏览器的默认行为
- * @param {*} argument 自定义传参
+ * @param {Object} argument 自定义传参
  * 示例 myEvent.dispatchEvent('召唤兵器', {name: 'jstools-common'})
  */
 const myEvent = {
@@ -245,6 +245,181 @@ const myEvent = {
   }
 }
 
+/**
+ * 网页截图并保存为PDF文件
+ * @param {Object} options 参数配置
+ * @param {String} options/catalogues 封面对应的dom元素数组
+ * @param {String} options/contents 内容页对应的dom元素数组
+ * @param {String} options/clipClass 可裁切元素的类名
+ * @param {String} options/flipClass 需另起一页绘制的元素的类名
+ * @param {String} options/fileName 要保存的截图名称
+ * @param {Number} options/paperWidth 有效宽度（默认参考A4纸，取值297）
+ * @param {Number} options/paperHeight 有效高度（默认参考A4纸，取值210）
+ * @param {Number} options/availWidth 有效宽度（默认参考A4纸，取值287）
+ * @param {Number} options/availHeight 有效高度（默认参考A4纸，取值185）
+ * @param {Number} options/pageStartTop 页面内容纵向开始位置（默认参考A4纸，取值10）
+ * @param {Number} options/pageStartLeft 页面内容横向开始位置（默认参考A4纸，取值5）
+ * @param {Number} options/pageNumTop 页码纵向位置（默认参考A4纸，取值280）
+ * @param {Number} options/pageNumLeft 页码横向位置（默认参考A4纸，取值205）
+ * @param {Number} options/fontSize 文本字号大小（默认12）
+ */
+
+const screenshot2pdf = (options = {}) => {
+  options = options || {}
+  let html2canvas = options.html2canvas
+  let jsPDF = options.jsPDF
+  let catalogues = [...(options.catalogues || [])]
+  let contents = [...(options.contents || [])]
+  let clipClass = options.clipClass || ''
+  let flipClass = options.flipClass || ''
+  let fileName = options.fileName || 'screenshot'
+  let paperWidth = options.paperWidth || 297
+  let paperHeight = options.paperHeight || 210
+  let availWidth = options.availWidth || 287
+  let availHeight = options.availHeight || 185
+  let pageStartTop = options.pageStartTop || 10
+  let pageStartLeft = options.pageStartLeft || 5
+  let pageNumTop = options.pageNumTop || 280
+  let pageNumLeft = options.pageNumLeft || 205
+  let fontSize = options.fontSize || 12
+  if (!html2canvas) return Promise.reject('need input plugin method: html2canvas')
+  if (!jsPDF) return Promise.reject('need input plugin method: jsPDF')
+  if (!catalogues.length && !contents.length) return Promise.reject('tasks is empty')
+  let _scrollTop = document.querySelector('html').scrollTop // 记录滚动条位置，程序执行完成后恢复
+  let _scale = [...catalogues, ...contents][0].getBoundingClientRect().width / availWidth // dom文档和纸张之间的比例
+  let _pageMaxHeight = _scale * availHeight // 每一页最大容纳的dom高度
+  let _pageUsedHeight = 0 // 当前页面已经绘制的图像高度
+  let _offset = 0
+  let cataloguesPageList = [[]]
+  let contentsPageList = [[]]
+  let cataloguesTaskList = []
+  let contentsTaskList = []
+  function doms2pages (doms) {
+    let pageList = [[]]
+    let taskList = []
+    doms.forEach((v, i) => {
+      let vInfo = v.getBoundingClientRect()
+      let {width, height} = vInfo
+      let canClip = clipClass ? [...v.classList].includes(clipClass) : false
+      let needFlip = flipClass ? [...v.classList].includes(flipClass) : false
+      if (canClip && height > _pageMaxHeight) {
+        let __ht = 0 // 已绘制高度
+        let __hb = height // 待绘制高度
+        let __s = _pageMaxHeight - _pageUsedHeight // 当前页面富余量
+        let __c = 0 // 裁切第几次
+        let __h = __s // 裁切高度
+        while (__hb > 0) {
+          // html2canvas
+          if (__c === 0) {
+            __h = needFlip ? _pageMaxHeight : __s
+          } else {
+            __h = Math.min(__hb, _pageMaxHeight)
+          }
+          taskList.push(
+            html2canvas(v, {
+              width, height: __h,
+              y: (vInfo.y || vInfo.top) + __ht,
+              x: vInfo.x || vInfo.left,
+              _scale, allowTaint: true
+            })
+          )
+          // page
+          if (_pageUsedHeight + __h <= _pageMaxHeight) {
+            pageList[pageList.length - 1].push(i + _offset)
+            _pageUsedHeight += __h
+          } else if (_pageUsedHeight === 0) {
+            pageList[pageList.length - 1].push(i + _offset)
+            pageList[pageList.length] = []
+          } else {
+            pageList[pageList.length] = []
+            pageList[pageList.length - 1].push(i + _offset)
+            _pageUsedHeight = __h
+          }
+          if (__hb > _pageMaxHeight) {
+            __c++
+            _offset++
+          }
+          __ht += __h
+          __hb -= __h
+        }
+      } else {
+        // html2canvas
+        taskList.push(
+          html2canvas(v, {
+            width, height,
+            y: vInfo.y || vInfo.top,
+            x: vInfo.x || vInfo.left,
+            _scale, allowTaint: true
+          })
+        )
+        // page
+        if (_pageUsedHeight + height <= _pageMaxHeight) {
+          pageList[pageList.length - 1].push(i + _offset)
+          _pageUsedHeight += height
+        } else if (_pageUsedHeight === 0) {
+          pageList[pageList.length - 1].push(i + _offset)
+          pageList[pageList.length] = []
+        } else {
+          pageList[pageList.length] = []
+          pageList[pageList.length - 1].push(i + _offset)
+          _pageUsedHeight = height
+        }
+      }
+    })
+    return {pageList, taskList}
+  }
+  document.querySelector('html').scrollTop = 0 // 截图前需将滚动条重置为0
+  let _resultCatalogues = doms2pages(catalogues)
+  cataloguesPageList = _resultCatalogues.pageList
+  cataloguesTaskList = _resultCatalogues.taskList
+  let _resultContents = doms2pages(contents)
+  contentsPageList = _resultContents.pageList
+  contentsTaskList = _resultContents.taskList
+  return Promise.all([...cataloguesTaskList, ...contentsTaskList]).then((canvasArr) => {
+    let canvasArrCatalogues = canvasArr.slice(0, cataloguesTaskList.length)
+    let canvasArrContents = canvasArr.slice(cataloguesTaskList.length)
+    let doc = new jsPDF('landscape')
+    let docIndex = 0
+    doc.setFontSize(fontSize)
+    /**
+     * PDF页面组装
+     * @param {Array} pageList 散装页面列表
+     * @param {Number} _x 页面内容横向开始位置
+     * @param {Number} _y 页面内容纵向开始位置
+     * @param {Boolean} isCoverPage 是否为封面
+     */
+    function pagesPack (pageList, canvasList, isCoverPage) {
+      pageList.forEach((v, i) => {
+        if (v.length === 0) return
+        if (docIndex > 0) doc.addPage()
+        let _x = pageStartLeft
+        let _y = pageStartTop
+        let _w = availWidth
+        if (isCoverPage) {
+          _x = _y = 0
+          _w = paperWidth
+        }
+        v.forEach(val => {
+          let canvas = canvasList[val]
+          let _r = canvas.width / _w
+          let _h = canvas.height / (_r || 1)
+          doc.addImage(canvas.toDataURL('image/jpeg'), 'JPEG', _x, _y, _w, _h)
+          _y += _h
+        })
+        if (!isCoverPage) {
+          doc.text(`${i + 1} / ${pageList.length}`, pageNumTop, pageNumLeft)
+        }
+        console.log(docIndex, 'pageList', v, i, pageList.length)
+        docIndex++
+      })
+    }
+    pagesPack(cataloguesPageList, canvasArrCatalogues, true)
+    pagesPack(contentsPageList, canvasArrContents, false)
+    doc.save(`${fileName}.pdf`)
+    document.querySelector('html').scrollTop = _scrollTop // 截图后将滚动条恢复
+  })
+}
+
 let toolsWeb = {
   ...toolsPublic,
   ...polyfill,
@@ -259,7 +434,8 @@ let toolsWeb = {
   base64Decode,
   dataUrl2File,
   getUrlParams,
-  myEvent
+  myEvent,
+  screenshot2pdf
 }
 
 module.exports = (jstools) => {
